35592addaa78d53e8f430c828a6099f184b28b5f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-09-21 19:03:14 +0300

    Initial commit

---
+++ 03.Sets and Dictionaries Advanced Exercise/03.Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCnt = int.Parse(Console.ReadLine());
+
+            SortedSet<string> set = new SortedSet<string>();
+
+            for (int line = 0; line < inputCnt; line++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                for (int element = 0; element < tokens.Length; element++)
+                {
+                    set.Add(tokens[element]);
+                }
+            }            
+            Console.WriteLine(String.Join(" ", set));
+        }
+    }
+}
