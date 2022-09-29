be566724b743054d749665e84d79dfe56cb14722
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-09-29 10:10:44 +0300

    Initial commit

---
+++ 05.Functional Programming Exercise/07.Predicate for Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Predicate_for_Names
+{
+    internal class Program
+    {
+        static Func<string, int, bool> isEqual = (w, x) => w.Length <= x;
+        static void Main(string[] args)
+        {
+            int thresHold = int.Parse(Console.ReadLine());
+            Console.WriteLine(string.Join(Environment.NewLine,
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => isEqual(w, thresHold))
+                ));
+        }
+    }
+}
