64dfa16411c0ef65f8314c0d88cbfd8f5d7068f0
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-09-30 10:16:47 +0300

    Initial commit

---
+++ 05.Functional Programming Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static Func<string, int, bool> isValid = (s, n) => s.ToCharArray().Sum(ch => (int)ch) >= n;
+        static Action<Func<string, int, bool>, string[], int> PrintValid => (f, arr, x) =>
+        {
+            Console.WriteLine(arr.FirstOrDefault(w => f(w, x)));
+        };
+        static void Main(string[] args)
+        {
+            int thresHold = int.Parse(Console.ReadLine());
+            PrintValid(isValid, Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries), thresHold);
+        }
+    }
+}
