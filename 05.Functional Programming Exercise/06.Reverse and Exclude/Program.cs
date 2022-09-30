2ad498424fa78671c177146376915588d9a7f75c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-09-30 12:22:41 +0300

    updating solution

--- 05.Functional Programming Exercise/06.Reverse and Exclude/Program.cs
+++ 05.Functional Programming Exercise/06.Reverse and Exclude/Program.cs
@@ -5,7 +5,7 @@ namespace _06.Reverse_and_Exclude
 {
     internal class Program
     {
-        static Func<int, int, bool> isDevisable = (x, y) => x % y != 0;
+        static Func<int, int, bool> isNotDevisable = (x, y) => x % y != 0;
         static void Main(string[] args)
         {
             var inputArr = Console.ReadLine()
@@ -15,7 +15,7 @@ namespace _06.Reverse_and_Exclude
             int divider = int.Parse(Console.ReadLine());
 
             Console.WriteLine(String.Join(" ", inputArr
-                .Where(x => isDevisable(x, divider))
+                .Where(x => isNotDevisable(x, divider))
                 .Reverse()));
                
         }
