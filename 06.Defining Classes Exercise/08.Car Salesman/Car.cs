e53fe50eb3f5e98388d92dfef61748dcad2fafb3
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-03 13:29:34 +0300

    Initial commit

---
+++ 06.Defining Classes Exercise/08.Car Salesman/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+        public Car()
+        {
+            Weight = -1;
+            Color = "n/a";
+        }
+        public Car(string model, Engine engine):this()
+        {
+            Model = model;
+            Engine = engine;           
+        }
+        public Car(string model, Engine engine, int weight) : this(model, engine)
+        {
+            Weight = weight;            
+        }
+        public Car(string model, Engine engine, string color) : this(model, engine)
+        {
+            Color = color;
+        }
+        public Car(string model, Engine engine, int weight, string color) : this(model, engine, weight)
+        {
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Model}:");
+            sb.AppendLine($"{this.Engine}");
+            if (this.Weight == -1)
+            {
+                sb.AppendLine("  Weight: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"  Weight: {this.Weight}");
+            }
+            sb.Append($"  Color: {this.Color}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 06.Defining Classes Exercise/08.Car Salesman/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.CarSalesman
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Engine> engines = new List<Engine>();
+            int enginesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < enginesCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length == 2)
+                {
+                    Engine engine = new Engine(tokens[0], int.Parse(tokens[1]));
+                    engines.Add(engine);
+                }
+                else if (tokens.Length == 3)
+                {
+                    int dissplacement = 0;
+                    bool isInt = int.TryParse(tokens[2], out dissplacement);
+                    if (isInt)
+                    {
+                        Engine engine = new Engine(tokens[0], int.Parse(tokens[1]), dissplacement);
+                        engines.Add(engine);
+                    }
+                    else
+                    {
+                        Engine engine = new Engine(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                        engines.Add(engine);
+                    }
+                }
+                else
+                {
+                    Engine engine = new Engine(tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2]), tokens[3]);
+                    engines.Add(engine);
+                }
+            }
+
+            List<Car> cars = new List<Car>();
+            int carsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens.Length == 2)
+                {
+                    Engine engine = engines.Find(e => e.Model == tokens[1]);
+                    Car car = new Car(tokens[0], engine);
+                    cars.Add(car);
+                }
+                else if (tokens.Length == 3)
+                {
+                    Engine engine = engines.Find(e => e.Model == tokens[1]);
+
+                    int weight = 0;
+                    bool isInt = int.TryParse(tokens[2], out weight);
+                    if (isInt)
+                    {
+                        Car car = new Car(tokens[0], engine, weight);
+                        cars.Add(car);
+                    }
+                    else
+                    {
+                        Car car = new Car(tokens[0], engine, tokens[2]);
+                        cars.Add(car);
+                    }
+                }
+                else
+                {
+                    Engine engine = engines.Find(e => e.Model == tokens[1]);
+                    Car car = new Car(tokens[0], engine, int.Parse(tokens[2]), tokens[3]);
+                    cars.Add(car);
+                }
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
