7cda4ea5dc2d0fc475e2dfb084153fd76eacf270
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-04 19:34:29 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Exam - 25 June 2022/01.Tiles Master/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] whiteTilesInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] grayTilesInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> whiteTiles = new Stack<int>();
+            Queue<int> grayTiles = new Queue<int>();
+
+            for (int i = 0; i < whiteTilesInfo.Length; i++)
+            {
+                whiteTiles.Push(whiteTilesInfo[i]);
+            }
+            for (int i = 0; i < grayTilesInfo.Length; i++)
+            {
+                grayTiles.Enqueue(grayTilesInfo[i]);
+            }
+
+            Dictionary<int, string> tilesReq = new Dictionary<int, string>();
+            tilesReq.Add(40, "Sink");
+            tilesReq.Add(50, "Oven");
+            tilesReq.Add(60, "Countertop");
+            tilesReq.Add(70, "Wall");
+
+            Dictionary<string, int> tilesDone = new Dictionary<string, int>();
+
+            while (whiteTiles.Any() && grayTiles.Any())
+            {
+                int curWhiteTile = whiteTiles.Peek();
+                int curGrayTile = grayTiles.Peek();
+
+                if (curWhiteTile == curGrayTile)
+                {
+                    int newTileSum = curWhiteTile + curGrayTile;
+                    if (tilesReq.ContainsKey(newTileSum))
+                    {
+                        string curNewTile = tilesReq[newTileSum];
+                        if (!tilesDone.Any(t => t.Key == curNewTile))
+                        {
+                            tilesDone.Add(curNewTile, 0);
+                        }
+                        tilesDone[curNewTile]++;
+                    }
+                    else
+                    {
+                        if (!tilesDone.Any(t => t.Key == "Floor"))
+                        {
+                            tilesDone.Add("Floor", 0);
+                        }
+                        tilesDone["Floor"]++;
+                    }
+                    whiteTiles.Pop();
+                    grayTiles.Dequeue();
+                }
+                else
+                {
+                    whiteTiles.Push(whiteTiles.Pop() / 2);
+                    grayTiles.Enqueue(grayTiles.Dequeue());                    
+                }
+            }
+
+            if (!whiteTiles.Any())
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ", whiteTiles)}");
+            }
+            if (!grayTiles.Any())
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", grayTiles)}");
+            }
+
+            foreach (var tile in tilesDone.OrderByDescending(t => t.Value).ThenBy(t => t.Key))
+            {
+                Console.WriteLine($"{tile.Key}: {tile.Value}");
+            }
+        }
+    }
+}
---
+++ Exam Preparation/C# Advanced Exam - 25 June 2022/02.Wall Destroyer/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace _02.Wall_Destroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            int playerRow = 0;
+            int playerCol = 0;
+            
+            for (int row = 0; row < size; row++)
+            {
+                char[] curRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < size; col++)
+                {                    
+                    if (curRow[col] == 'V')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                    matrix[row, col] = curRow[col];
+                }
+            }
+
+            string command;
+            bool isElectrocuted = false;
+            int holesMade = 1;
+            int rodsHitted = 0;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                int newPlayerRow = playerRow;
+                int newPlayerCol = playerCol;
+                bool hasplayerMoved = false;
+                bool isWallAlreadyDestroyed = false;
+                
+                switch (command)
+                {
+                    case "up":
+                        if (isValidMove(newPlayerRow - 1, newPlayerCol, matrix))
+                        {
+                            newPlayerRow--;
+                            MovePlayer(newPlayerRow, newPlayerCol, matrix, ref isElectrocuted, ref hasplayerMoved, ref rodsHitted, ref isWallAlreadyDestroyed);
+                        }
+                        break;
+                    case "down":
+                        if (isValidMove(newPlayerRow + 1, newPlayerCol, matrix))
+                        {
+                            newPlayerRow++;
+                            MovePlayer(newPlayerRow, newPlayerCol, matrix, ref isElectrocuted, ref hasplayerMoved, ref rodsHitted, ref isWallAlreadyDestroyed);
+                        }
+                        break;
+                    case "left":
+                        if (isValidMove(newPlayerRow, newPlayerCol - 1, matrix))
+                        {
+                            newPlayerCol--;
+                            MovePlayer(newPlayerRow, newPlayerCol, matrix, ref isElectrocuted, ref hasplayerMoved, ref rodsHitted, ref isWallAlreadyDestroyed);
+                        }
+                        break;
+                    case "right":
+                        if (isValidMove(newPlayerRow, newPlayerCol + 1, matrix))
+                        {
+                            newPlayerCol++;
+                            MovePlayer(newPlayerRow, newPlayerCol, matrix, ref isElectrocuted, ref hasplayerMoved, ref rodsHitted, ref isWallAlreadyDestroyed);
+                        }
+                        break;
+                }
+
+                if (isElectrocuted)
+                {
+                    matrix[playerRow, playerCol] = '*';
+                    holesMade++;
+                    break;
+                }
+
+                if (hasplayerMoved == true)
+                {
+                    if (!isWallAlreadyDestroyed)
+                    {
+                        holesMade++;
+                    }
+                    matrix[playerRow, playerCol] = '*';                    
+                    playerRow = newPlayerRow;
+                    playerCol = newPlayerCol;
+                }               
+            }
+
+            if (!isElectrocuted)
+            {
+                Console.WriteLine($"Vanko managed to make {holesMade} hole(s) and he hit only {rodsHitted} rod(s).");
+            }
+            else
+            {
+                Console.WriteLine($"Vanko got electrocuted, but he managed to make {holesMade} hole(s).");
+            }
+            PrintMatrix(matrix);
+        }
+
+        private static void MovePlayer(int newPlayerRow, int newPlayerCol, char[,] matrix, ref bool isElectrocuted, ref bool hasplayerMoved, ref int rodsHitted, ref bool isWallAlreadyDestroyed)
+        {
+            if (matrix[newPlayerRow, newPlayerCol] == 'R')
+            {
+                Console.WriteLine("Vanko hit a rod!");
+                rodsHitted++;
+                return;
+            }
+            else if (matrix[newPlayerRow, newPlayerCol] == 'C')
+            {
+                isElectrocuted = true;
+                hasplayerMoved = true;
+                matrix[newPlayerRow, newPlayerCol] = 'E';
+                return;
+            }
+            else if (matrix[newPlayerRow, newPlayerCol] == '*')
+            {
+                matrix[newPlayerRow, newPlayerCol] = 'V';
+                hasplayerMoved = true;
+                isWallAlreadyDestroyed = true;
+                Console.WriteLine($"The wall is already destroyed at position [{newPlayerRow}, {newPlayerCol}]!");
+                return;
+            }
+            else
+            {
+                hasplayerMoved = true;
+                matrix[newPlayerRow, newPlayerCol] = 'V';
+            }
+        }
+
+        private static bool isValidMove(int playerRow, int playerCol, char[,] matrix)
+        {
+            if (playerRow >= 0 && playerRow < matrix.GetLength(0)
+                && playerCol >= 0 && playerCol < matrix.GetLength(1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
