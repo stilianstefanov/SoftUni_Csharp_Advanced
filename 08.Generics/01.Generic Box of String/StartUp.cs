e2c1592e150045c4e1a43a1c6a0cd99d9609f914
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-08 16:09:10 +0300

    Initial commit

---
+++ 08.Generics/01.Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int inputCnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCnt; i++)
+            {
+                string input = Console.ReadLine();
+
+                Console.WriteLine(new Box<string>(input));
+            }
+        }
+    }
+}
