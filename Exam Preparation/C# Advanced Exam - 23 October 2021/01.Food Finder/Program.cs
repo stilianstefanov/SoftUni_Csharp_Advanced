49b4be9efc83b04984e8c5a4d1d5e68a0f931bbc
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-10 13:13:29 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Exam - 23 October 2021/01.Food Finder/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowelsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            char[] consonantsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(char.Parse)
+               .ToArray();
+
+            Queue<char> vowels = new Queue<char>();
+            for (int i = 0; i < vowelsInfo.Length; i++)
+            {
+                vowels.Enqueue(vowelsInfo[i]);
+            }
+            Stack<char> consonants = new Stack<char>();
+            for (int i = 0; i < consonantsInfo.Length; i++)
+            {
+                consonants.Push(consonantsInfo[i]);
+            }
+
+            Dictionary<string, Dictionary<char, bool>> words = new Dictionary<string, Dictionary<char, bool>>();
+
+            words.Add("pear", new Dictionary<char, bool>());
+            words.Add("flour", new Dictionary<char, bool>());
+            words.Add("pork", new Dictionary<char, bool>());
+            words.Add("olive", new Dictionary<char, bool>());
+
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Key.Length; i++)
+                {
+                    word.Value.Add(word.Key[i], false);
+                }
+            }
+
+            while (consonants.Any())
+            {
+                char currentVowel = vowels.Dequeue();
+                char currentConsonant = consonants.Pop();
+
+                foreach (var word in words)
+                {
+                    if (word.Key.Contains(currentVowel))
+                    {
+                        word.Value[currentVowel] = true;
+                    }
+                }
+                foreach (var word in words)
+                {
+                    if (word.Key.Contains(currentConsonant))
+                    {
+                        word.Value[currentConsonant] = true;
+                    }
+                }
+
+                vowels.Enqueue(currentVowel);
+            }
+
+            List<string> wordsFound = new List<string>();
+
+            foreach (var word in words)
+            {
+                bool areAllCharsFound = true;
+                foreach (var ch in word.Value)
+                {
+                    if (ch.Value == false)
+                    {
+                        areAllCharsFound = false;
+                        break;
+                    }
+                }
+                if (areAllCharsFound)
+                {
+                    wordsFound.Add(word.Key);
+                }
+            }
+
+            Console.WriteLine($"Words found: {wordsFound.Count}");
+            Console.WriteLine(string.Join(Environment.NewLine, wordsFound));
+        }
+    }
+}
---
+++ Exam Preparation/C# Advanced Exam - 23 October 2021/02.Pawn Wars/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Pawn_Wars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 8;
+
+            char[,] matrix = new char[size, size];
+
+            int whitePawnRow = 0;
+            int whitePawnCol = 0;
+
+            int blackPawnRow = 0;
+            int blackPawnCol = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                char[] curRow = Console.ReadLine().ToCharArray();
+                    
+                for (int col = 0; col < size; col++)
+                {
+                    if (curRow[col] == 'w')
+                    {
+                        whitePawnRow = row;
+                        whitePawnCol = col;
+                    }
+                    if (curRow[col] == 'b')
+                    {
+                        blackPawnRow = row;
+                        blackPawnCol = col;
+                    }
+                    matrix[row, col] = curRow[col];
+                }
+            }
+
+            Dictionary<int, string> rowValidation = new Dictionary<int, string>();
+            rowValidation.Add(0, "8");
+            rowValidation.Add(1, "7");
+            rowValidation.Add(2, "6");
+            rowValidation.Add(3, "5");
+            rowValidation.Add(4, "4");
+            rowValidation.Add(5, "3");
+            rowValidation.Add(6, "2");
+            rowValidation.Add(7, "1");
+            Dictionary<int, string> columnValidation = new Dictionary<int, string>();
+            columnValidation.Add(0, "a");
+            columnValidation.Add(1, "b");
+            columnValidation.Add(2, "c");
+            columnValidation.Add(3, "d");
+            columnValidation.Add(4, "e");
+            columnValidation.Add(5, "f");
+            columnValidation.Add(6, "g");
+            columnValidation.Add(7, "h");
+           
+            int counter = 0;
+
+            while (true)
+            {
+                if (counter % 2 == 0)
+                {
+                    if (whitePawnRow - 1 >= 0 && whitePawnCol - 1 >= 0) // Left
+                    {
+                        if (matrix[whitePawnRow - 1, whitePawnCol - 1] == 'b')
+                        {
+                            Console.WriteLine($"Game over! White capture on {columnValidation[whitePawnCol - 1]}{rowValidation[whitePawnRow - 1]}.");
+                            return;
+                        }
+                    }
+                    if (whitePawnRow - 1 >= 0 && whitePawnCol + 1 < size) // Right
+                    {
+                        if (matrix[whitePawnRow - 1, whitePawnCol + 1] == 'b')
+                        {
+                            Console.WriteLine($"Game over! White capture on {columnValidation[whitePawnCol + 1]}{rowValidation[whitePawnRow - 1]}.");
+                            return;
+                        }
+                    }
+                    matrix[whitePawnRow, whitePawnCol] = '-';
+                    matrix[whitePawnRow - 1, whitePawnCol] = 'w';
+
+                    whitePawnRow--;
+                    if (whitePawnRow == 0)
+                    {
+                        Console.WriteLine($"Game over! White pawn is promoted to a queen at {columnValidation[whitePawnCol]}{rowValidation[whitePawnRow]}.");
+                        return;
+                    }
+                }
+                else
+                {
+                    if (blackPawnRow + 1 < size && blackPawnCol - 1 >= 0) // Left
+                    {
+                        if (matrix[blackPawnRow + 1, blackPawnCol - 1] == 'w')
+                        {
+                            Console.WriteLine($"Game over! Black capture on {columnValidation[blackPawnCol - 1]}{rowValidation[blackPawnRow + 1]}.");
+                            return;
+                        }
+                    }
+                    if (blackPawnRow + 1 < size && blackPawnCol + 1 < size) // Right
+                    {
+                        if (matrix[blackPawnRow + 1, blackPawnCol + 1] == 'w')
+                        {
+                            Console.WriteLine($"Game over! Black capture on {columnValidation[blackPawnCol + 1]}{rowValidation[blackPawnRow + 1]}.");
+                            return;
+                        }
+                    }
+                    matrix[blackPawnRow, blackPawnCol] = '-';
+                    matrix[blackPawnRow + 1, blackPawnCol] = 'b';
+
+                    blackPawnRow++;
+                    if (blackPawnRow == size - 1)
+                    {
+                        Console.WriteLine($"Game over! Black pawn is promoted to a queen at {columnValidation[blackPawnCol]}{rowValidation[blackPawnRow]}.");
+                        return;
+                    }
+                }
+
+                counter++;
+            }
+        }
+
+    }
+}
