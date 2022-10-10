182e233a50d7f7b5fc81a28602b7f4e80755e8c8
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-10 20:26:26 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Exam - 23 October 2021/03.Stock Market/Investor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            Portfolio = new List<Stock>();
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+        }
+
+        public List<Stock> Portfolio { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+
+        public int Count { get { return this.Portfolio.Count; } }
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000)
+            {
+                if (this.MoneyToInvest - stock.PricePerShare >= 0)
+                {
+                    this.MoneyToInvest -= stock.PricePerShare;
+                    this.Portfolio.Add(stock);
+                }
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            if (!this.Portfolio.Any(s => s.CompanyName == companyName))
+            {
+                return $"{companyName} does not exist.";
+            }
+            Stock stockToSell = this.Portfolio.Find(s => s.CompanyName == companyName);
+            if (sellPrice < stockToSell.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+            else
+            {
+                this.MoneyToInvest += sellPrice;
+                this.Portfolio.Remove(stockToSell);
+
+                return $"{companyName} was sold.";
+            }
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            Stock stock = this.Portfolio.FirstOrDefault(s => s.CompanyName == companyName);
+
+            return stock;
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            if (this.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return this.Portfolio.OrderByDescending(s => s.MarketCapitalization).First();
+            }
+        }
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {this.FullName} with a broker {this.BrokerName} has stocks:");
+            foreach (var stock in this.Portfolio)
+            {
+                sb.AppendLine($"{stock.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Preparation/C# Advanced Exam - 23 October 2021/03.Stock Market/Stock.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+        }
+
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization { get { return PricePerShare * TotalNumberOfShares; } }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Company: {this.CompanyName}");
+            sb.AppendLine($"Director: {this.Director}");
+            sb.AppendLine($"Price per share: ${this.PricePerShare}");
+            sb.Append($"Market capitalization: ${this.MarketCapitalization}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
