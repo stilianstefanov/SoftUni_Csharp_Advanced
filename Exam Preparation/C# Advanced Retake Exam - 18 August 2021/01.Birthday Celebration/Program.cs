440a47dd0bd5bbeb4c735f70be6fe5a8854c3218
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-11 10:03:55 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Retake Exam - 18 August 2021/01.Birthday Celebration/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Birthday_Celebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] guestsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] platesInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            Queue<int> guests = new Queue<int>();
+            for (int i = 0; i < guestsInfo.Length; i++)
+            {
+                guests.Enqueue(guestsInfo[i]);
+            }
+            Stack<int> plates = new Stack<int>();
+            for (int i = 0; i < platesInfo.Length; i++)
+            {
+                plates.Push(platesInfo[i]);
+            }
+
+            int wastedFood = 0;
+
+            while (guests.Any() && plates.Any())
+            {
+                int currentGuest = guests.Dequeue();
+                while (currentGuest > 0)
+                {
+                    int currentPlate = plates.Pop();
+                    currentGuest -= currentPlate;
+
+                    if (currentGuest < 0)
+                    {
+                        wastedFood += Math.Abs(currentGuest);
+                    }
+                }
+            }
+
+            if (plates.Any())
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", plates)}");
+            }
+            else if (guests.Any())
+            {
+                Console.WriteLine($"Guests: {string.Join(" ", guests)}");
+            }
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
