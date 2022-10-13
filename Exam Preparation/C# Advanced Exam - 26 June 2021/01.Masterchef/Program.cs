fb118196c5e39b3e448fc2de8f5409fb57267c6f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-13 11:11:17 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Exam - 26 June 2021/01.Masterchef/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ingredientsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] freshnessInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> ingredients = new Queue<int>();
+            for (int i = 0; i < ingredientsInfo.Length; i++)
+            {
+                ingredients.Enqueue(ingredientsInfo[i]);
+            }
+
+            Stack<int> freshness = new Stack<int>();
+            for (int i = 0; i < freshnessInfo.Length; i++)
+            {
+                freshness.Push(freshnessInfo[i]);
+            }
+
+            Dictionary<string, int> dishesInfo = new Dictionary<string, int>();
+            dishesInfo.Add("Dipping sauce", 150);
+            dishesInfo.Add("Green salad", 250);
+            dishesInfo.Add("Chocolate cake", 300);
+            dishesInfo.Add("Lobster", 400);
+
+            Dictionary<string, int> dishesMade = new Dictionary<string, int>();
+
+            while (ingredients.Any() && freshness.Any())
+            {
+                while (ingredients.Any() && ingredients.Peek() == 0)
+                {
+                    ingredients.Dequeue();
+                }
+                
+                if (!ingredients.Any() || !freshness.Any())
+                {
+                    break;
+                }
+
+                int currentIngredient = ingredients.Peek();
+                int currentFreshness = freshness.Pop();
+                int result = currentIngredient * currentFreshness;
+
+                if (dishesInfo.ContainsValue(result))
+                {
+                    string dishMade = dishesInfo.First(d => d.Value == result).Key;
+                    if (!dishesMade.Any(d => d.Key == dishMade))
+                    {
+                        dishesMade.Add(dishMade, 0);
+                    }
+                    dishesMade[dishMade]++;
+                    ingredients.Dequeue();
+                }
+                else
+                {
+                    ingredients.Enqueue(ingredients.Dequeue() + 5);
+                }
+            }
+
+            if (dishesMade.Count == 4)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+            if (dishesMade.Count > 0)
+            {
+                foreach (var item in dishesMade.OrderBy(d => d.Key))
+                {
+                    Console.WriteLine($" # {item.Key} --> {item.Value}");
+                }
+            }            
+        }
+    }
+}
---
+++ Exam Preparation/C# Advanced Exam - 26 June 2021/02.Survivor/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Survivor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCnt = int.Parse(Console.ReadLine());
+
+            char[][] jaggedArr = new char[rowsCnt][];
+
+            for (int row = 0; row < rowsCnt; row++)
+            {
+                char[] curRow = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+                jaggedArr[row] = curRow;
+            }
+
+            int collectedTokens = 0;
+            int opponentTokens = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "Gong")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (tokens[0])
+                {
+                    case "Find":
+                        {
+                            int row = int.Parse(tokens[1]);
+                            int col = int.Parse(tokens[2]);
+                            if (row >= 0 && row < rowsCnt && col >= 0 && col < jaggedArr[row].Length)
+                            {
+                                if (jaggedArr[row][col] == 'T')
+                                {
+                                    collectedTokens++;
+                                    jaggedArr[row][col] = '-';
+                                }
+                            }
+                            break;
+                        }
+                    case "Opponent":
+                        {
+                            int row = int.Parse(tokens[1]);
+                            int col = int.Parse(tokens[2]);
+                            string direction = tokens[3];
+                            if (row >= 0 && row < rowsCnt && col >= 0 && col < jaggedArr[row].Length)
+                            {
+                                if (jaggedArr[row][col] == 'T')
+                                {
+                                    opponentTokens++;
+                                    jaggedArr[row][col] = '-';
+                                }
+                                MoveOpponent(jaggedArr, row, col, direction, ref opponentTokens);
+                            }
+
+                            break;
+                        }
+                }
+            }
+            Print(jaggedArr);
+            Console.WriteLine($"Collected tokens: {collectedTokens}");
+            Console.WriteLine($"Opponent's tokens: {opponentTokens}");
+        }
+
+        private static void MoveOpponent(char[][] jaggedArr, int startRow, int startCol, string direction, ref int opponentTokens)
+        {
+            switch (direction)
+            {
+                case "up":
+                    {
+                        int counter = 0;
+                        for (int row = startRow; row >= 0; row--)
+                        {
+                            if (counter == 4)
+                            {
+                                break;
+                            }
+                            if (jaggedArr[row][startCol] == 'T')
+                            {
+                                opponentTokens++;
+                            }
+                            jaggedArr[row][startCol] = '-';
+                            counter++;
+                        }
+                        break;
+                    }
+                case "down":
+                    {
+                        int counter = 0;
+                        for (int row = startRow; row < jaggedArr.Length; row++)
+                        {
+                            if (counter == 4)
+                            {
+                                break;
+                            }
+                            if (jaggedArr[row][startCol] == 'T')
+                            {
+                                opponentTokens++;
+                            }
+                            jaggedArr[row][startCol] = '-';
+                            counter++;
+                        }
+                        break;
+                    }
+                case "left":
+                    {
+                        int counter = 0;
+                        for (int col = startCol; col >= 0; col--)
+                        {
+                            if (counter == 4)
+                            {
+                                break;
+                            }
+                            if (jaggedArr[startRow][col] == 'T')
+                            {
+                                opponentTokens++;
+                            }
+                            jaggedArr[startRow][col] = '-';
+                            counter++;
+                        }
+                        break;
+                    }
+                case "right":
+                    {
+                        int counter = 0;
+                        for (int col = startCol; col < jaggedArr[startRow].Length; col++)
+                        {
+                            if (counter == 4)
+                            {
+                                break;
+                            }
+                            if (jaggedArr[startRow][col] == 'T')
+                            {
+                                opponentTokens++;
+                            }
+                            jaggedArr[startRow][col] = '-';
+                            counter++;
+                        }
+                        break;
+                    }
+            }
+        }
+
+        private static void Print(char[][] jaggedArr)
+        {
+            for (int row = 0; row < jaggedArr.Length; row++)
+            {
+                char[] currentRow = jaggedArr[row];
+                Console.WriteLine(String.Join(" ", currentRow));
+            }
+        }
+    }
+}
