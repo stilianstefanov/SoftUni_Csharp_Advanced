8c724eab2d52c31062213df4ea75991b3b2f55b4
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-15 00:01:21 +0300

    Initial commit

---
+++ Exam Preparation/C# Advanced Exam - 22 Feb 2020/03.Guild/Guild.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roaster;
+
+        public Guild(string name, int capacity)
+        {
+            this.roaster = new List<Player>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get { return this.roaster.Count; } }
+
+        public void AddPlayer(Player player)
+        {
+            if (roaster.Count < this.Capacity)
+            {
+                roaster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            if (this.roaster.Any(p => p.Name == name))
+            {
+                this.roaster.Remove(this.roaster.First(p => p.Name == name));
+                return true;
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+            if (this.roaster.Any(p => p.Name == name))
+            {
+                Player player = this.roaster.First(p => p.Name == name);
+                if (player.Rank == "Member") return;
+
+                player.Rank = "Member";
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            if (this.roaster.Any(p => p.Name == name))
+            {
+                Player player = this.roaster.First(p => p.Name == name);
+                if (player.Rank == "Trial") return;
+
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] playerList = this.roaster.Where(p => p.Class == @class).ToArray();
+            this.roaster.RemoveAll(p => p.Class == @class);
+
+            return playerList;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var player in this.roaster)
+            {
+                sb.AppendLine($"{player.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Preparation/C# Advanced Exam - 22 Feb 2020/03.Guild/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            Name = name;
+            Class = @class;           
+        }
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; } = "Trial";
+        public string Description { get; set; } = "n/a";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Player {Name}: {Class}");
+            sb.AppendLine($"Rank: {Rank}");
+            sb.Append($"Description: {Description}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
