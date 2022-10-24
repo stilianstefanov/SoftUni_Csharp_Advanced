95a0eb96ac97c00c59aec7729ea2194a2369807f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-10-24 16:14:39 +0300

    Initial commit

---
+++ Algorithms Introduction/01.Recursive Array Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(GetSum(input, 0));
+        }
+
+        private static int GetSum(int[] input, int index)
+        {
+            if (index == input.Length)
+            {
+                return 0;
+            }
+            int result = input[index] + GetSum(input, index + 1);
+
+            return result;
+        }
+    }
+}
